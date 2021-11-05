59619ff68a7aad30547454dadb142ea4130a21e9
stepanvar <zsteve2911@gmail.com>
2021-11-05 22:16:56 +0300

    add little field

--- pyfiles/dicegame.py
+++ pyfiles/dicegame.py
@@ -1,6 +1,19 @@
 import random
-flag = 1
-def letters_in_log(name):
+import time
+flag = 7
+symb = '#' #input('enter symbol for borders\n')
+length = 3 #int(input('enter length of field\n'))
+width = 3 #int(input('enter width of field\n'))
+def border_vert_line(symb, width):
+    print (symb * width)
+def draw_field(symb:'symbol for walls', length, width, res_dice):
+    """ draw needed field"""
+    border_vert_line(symb, width)
+    for i in range(length - 2):
+        print(symb + '%d' %res_dice * (width - 2) + symb)
+    border_vert_line(symb, width)
+
+def letters_in_log(name:str()):
     name = list(name.lower())
     x = 2
     login_letters = random.choice(name)
@@ -11,28 +24,27 @@ def letters_in_log(name):
     return(login_letters)
 name = input("enter your name and i will generate your login\n")
 print("good name " + name + " but now we will make it better")
-print("wait...")
 login = letters_in_log(name) + str(random.randint(1, 77))
 print("Your login is", login, sep= " ")
 print("Hello, " + login + " let's play:")
 res_dice = random.randint(1, 10)
 while flag != 0:
-    while res_dice in range(1,7):
-        res_dice = random.randint(1, 10)
+        res_dice = random.randint(1, flag/2)
         if res_dice == 7:
             res_dice -= 3
-            print("your dice get %2d" %(res_dice), " continue")
+            print("your dice get:")
         elif res_dice == 8:
             res_dice -= 4
             print("your dice get %2d" %(res_dice) + " continue")
-        elif res_dice == 9:
-            print("your cheat dice get %d game over" %res_dice)
-        elif res_dice > 9:
-            res_dice -= 5
-            print("your dice get %2d" %(res_dice) + " continue")
+        # elif res_dice == 9:
+        #     print("your cheat dice get %d game over" %res_dice)
+        # elif res_dice > 9:
+        #     res_dice -= 5
+        #     print("your dice get %2d" %(res_dice) + " continue")
         else:
             print("your dice got " + str(res_dice) + " continue")
-    else:
+        draw_field(symb, length, width, res_dice)
+        flag += 1
         answer = int(input("wanna try 1 more time? 1 - yes, 2 - no\n"))
         if answer == 1:
             flag += 1
