a0e9b4d6b503ca416953db2f3987117ba2bdf791
Clydas Curie <ccurie@ox-m3.msk.21-school.ru>
2022-01-08 15:49:46 +0300

    reorganize pylanguage

--- pylanguage/pyfiles/pictures.py
+++ pylanguage/pyfiles/pictures.py
@@ -3,13 +3,12 @@ for i in range(4):
     square.append(square[i])
 for x in square:
     print(x, end='')
-symb = '#' #input('enter symbol for borders\n')
-length = 3 #int(input('enter length of field\n'))
-width = 3 #int(input('enter width of field\n'))
+symb = input('enter symbol for borders\n')
+length = int(input('enter length of field\n'))
+width = int(input('enter width of field\n'))
 def border_vert_line(symb, width):
     print (symb * width)
 def draw_field(symb:'symbol for walls', length, width):
-    """ draw needed field"""
     border_vert_line(symb, width)
     for i in range(length - 2):
         print(symb + ' ' * (width - 2) + symb)
--- pylanguage/pyfiles/report_fom.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy
-import openpyxl as px
-import re
-work = pd.read_excel("C:\\Downloads\\09_11.xlsx", sheet_name="Выгрузка по кодам")
-
-#work.to_excel('C:\\Downloads\\09_11_formate.xlsx', index=False)
