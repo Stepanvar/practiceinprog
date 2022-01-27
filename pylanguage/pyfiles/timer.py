9d84ccbbac83f4228ffc4b8573456c62833de4a5
stepanvar <zsteve2911@gmail.com>
2022-01-27 14:50:36 +0300

    commits in self_education

--- pylanguage/pyfiles/timer.py
+++ pylanguage/pyfiles/timer.py
@@ -10,35 +10,36 @@ count_z = []
 len = i + 1
 y = []
 while len > 0:
-    count_z.append(0)
-    len -= 1
+	count_z.append(0)
+	len -= 1
 while i > 0:
-    begin = datetime.now()
-    till_two_minutes = begin + timedelta(minutes=2)
-    while datetime.now() < till_two_minutes:
-        j = 0
-    winsound.Beep(random.randint(300,500),1000)
-    print("that's all number", i)
-    y.append(till_two_minutes)
-    file = open("D:/bioinfo/define.txt", encoding = 'utf-8')
-    try:
-        while True:
-            sym = file.read(1)
-            if sym == "Z":
-                count_z[index] += 1
-            elif sym == "":
-                break
-    finally:
-        file.close()
-    sent = []
-    sent.append(count_z[index] - count_z[index - 1])
-    print("number of sentences", count_z[index] - count_z[index - 1])
-    index += 1
-    i-=1
-print(count_z)
-while index > 0:
-    y.append(j)
-    j +=1
-    index -= 1
-plt.plot(y, sent)
-plt.show()
+	begin = datetime.now()
+	till_two_minutes = begin + timedelta(minutes=2)
+	while datetime.now() < till_two_minutes:
+		j = 0
+	winsound.Beep(random.randint(300,500),1000)
+	print("that's all number", i)
+	y.append(till_two_minutes)
+	i -= 1;
+#     file = open("D:/bioinfo/define.txt", encoding = 'utf-8')
+#     try:
+#         while True:
+#             sym = file.read(1)
+#             if sym == "Z":
+#                 count_z[index] += 1
+#             elif sym == "":
+#                 break
+#     finally:
+#         file.close()
+#     sent = []
+#     sent.append(count_z[index] - count_z[index - 1])
+#     print("number of sentences", count_z[index] - count_z[index - 1])
+#     index += 1
+#     i-=1
+# print(count_z)
+# while index > 0:
+#     y.append(j)
+#     j +=1
+#     index -= 1
+# plt.plot(y, sent)
+# plt.show()
